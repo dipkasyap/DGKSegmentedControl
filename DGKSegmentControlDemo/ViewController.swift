ac2d1e4da7b14fa2535b014607754bdccbdcb6a0
dip kasyap <dpd.ghimire@gmail.com>
2018-03-12 14:32:23 +0545

    Initial Commit

---
+++ DGKSegmentControlDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DGKSegmentControlDemo
+//
+//  Created by dip kasyap on 3/12/18.
+//  Copyright © 2018 dip kasyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
